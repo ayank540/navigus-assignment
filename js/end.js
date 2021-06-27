a03888c5d9e6748e76c299ce8b102da8301e5a4b
Ayan Khan <42548565+ayank540@users.noreply.github.com>
2021-06-27 12:02:19 +0530

    Front end things

---
+++ js/end.js
@@ -0,0 +1,29 @@
+const username = document.getElementById('username')
+const saveScoreBtn = document.getElementById('saveScoreBtn')
+const finalScore = document.getElementById('finalScore')
+const mostRecentScore = localStorage.getItem('mostRecentScore')
+
+const highScores = JSON.parse(localStorage.getItem('highScores')) || []
+
+finalScore.innerText = mostRecentScore
+
+username.addEventListener('keyup', () => {
+    saveScoreBtn.disabled = !username.value
+})
+
+saveHighScore = (e) => {
+    console.log("yeah it is clicked!")
+    e.preventDefault()
+
+    const score = {
+        score: mostRecentScore,
+        name: username.value
+    }
+    highScores.push(score)
+    highScores.sort((a, b) => {
+        return b.score - a.score
+    })
+    highScores.splice(5)
+    localStorage.setItem('highScores', JSON.stringify(highScores))
+    window.location.assign("/")
+}
